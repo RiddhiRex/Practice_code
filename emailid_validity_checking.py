925a996e659615b9a63d1baa88615b8ae832333c
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-08-23 13:01:46 -0500

    Update emailid_validity_checking.py

--- emailid_validity_checking.py
+++ emailid_validity_checking.py
@@ -1,3 +1,13 @@
+"""
+Write a program that uses regular expressions to determine if a string is a valid email address. 
+For this challenge, let's define a valid email address as name@domain.tld where "name" can contain the characters a-z, A-Z, 0-9, _, and
+. (dot). And "domain" can contain the characters a-z, and -. "name" and "domain" must be non-empty. "tld" can be "com", "org", or "net".
+Input:
+Your program should read lines of text from standard input. Each line contains a string that may or may not be a valid email address.
+Output:
+For each input string, print 'true' to standard output if the string is a valid email address as defined in this challenge. 
+Otherwise print 'false'. Print one string per line.
+"""
 import re
 def isValidEmail(email):
  if len(email) > 7:
