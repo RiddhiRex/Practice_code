6be2c640267d9ebf379584608e3169a3abec424d
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-10-16 22:29:54 -0400

    Update Implement queue with 2 stacks.py

--- Implement queue with 2 stacks.py
+++ Implement queue with 2 stacks.py
@@ -30,3 +30,36 @@ print(self.dequeue())
 self.enqueue(5)
 self.enqueue(6)
 print(self.dequeue())
+
+#method 2 (efficient)
+#give 2 stacks, implement a queue 
+class queue:
+    def __init__(self):
+        self.stack1=[] #for enqueue
+        self.stack2=[] #for dequeue
+
+    def enqueue(self, val):
+        self.stack1.append(val)
+
+    def dequeue(self):
+        if not self.stack2:
+            if(len(self.stack1)==0):
+                return None
+            while(len(self.stack1)>0):
+                self.stack2.append(self.stack1.pop())
+        val = self.stack2.pop()
+        return val
+            
+self = queue()
+self.enqueue(4)
+self.enqueue(5)
+self.enqueue(6)
+print(self.dequeue())
+print(self.dequeue()) 
+print(self.dequeue())
+print(self.dequeue())
+self.enqueue(5)
+self.enqueue(6)
+print(self.dequeue())
+        
+    
