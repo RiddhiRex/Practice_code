4bba3d284ee1eb21c6866d3b95d7fb39f730dc78
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-11-03 23:14:08 -0400

    Create memmove implementation.c

---
+++ memmove implementation.c
@@ -0,0 +1,31 @@
+#include<stdio.h> 
+#include<string.h> 
+  
+// A function to copy block of 'n' bytes from source 
+// address 'src' to destination address 'dest'. 
+void myMemMove(void *dest, void *src, int len) 
+{ 
+   // Typecast src and dest addresses to (char *) 
+   char *csrc = (char *)src; 
+   char *cdest = (char *)dest; 
+   char *temp = (char *)malloc(sizeof(len));
+  
+   // Copy data from csrc[] to temp[] 
+   for (int i=0; i<len; i++) 
+       temp[i] = csrc[i]; 
+  
+   // Copy data from temp[] to cdest[] 
+   for (int i=0; i<len; i++) 
+       cdest[i] = temp[i]; 
+  
+   free(temp);
+} 
+
+int main() 
+{ 
+   char src_st[100] = "asdf"; 
+   char dest_st[100];
+   myMemMove(dest_st, src_st, strlen(src_st)+1); 
+   printf("%s", dest_st); 
+   return 0; 
+}
